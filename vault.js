c42297525ea7692ec2d34458b602b774344e84df
BK <bkam03@yahoo.com>
2017-05-18 19:18:03 -1000

    vault getValue function validations working

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+  //prV
+  var vault = {};
+  //prM
+
+  //puV
+
+  //puM
+  function getValue(key) {
+    if(key === undefined) {
+      return null;
+    } else if(!(key in vault)) {
+      return null;
+    } else {
+      return vault[key];
+    }
+
+
+  }
+
+  return {
+    getValue : getValue,
+    //setValue : setValue
+  };
 };
